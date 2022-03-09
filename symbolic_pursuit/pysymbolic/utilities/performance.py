bfbfc2c3d0143c04c930222f46f329fa5fe2eaaa
Bogdan Cebere <bogdan.cebere@gmail.com>
2022-03-09 15:44:50 +0000

    cleanup + python wheel

--- symbolic_pursuit/pysymbolic/utilities/performance.py
+++ symbolic_pursuit/pysymbolic/utilities/performance.py
@@ -3,54 +3,54 @@
 
 from __future__ import absolute_import, division, print_function
 
-import sys, os, time
+import itertools
+import os
+import sys
+import time
+import warnings
+
 import numpy as np
 import pandas as pd
 import scipy as sc
-import itertools
-
 from mpmath import *
 from sympy import *
 
-from scipy.optimize import minimize
-
-import warnings
 warnings.filterwarnings("ignore")
 if not sys.warnoptions:
     warnings.simplefilter("ignore")
 
 from pysymbolic.models.special_functions import MeijerG
 
+
 def compute_Rsquared(f_true, f_est):
 
-	R2 = 1 - (np.mean((f_true - f_est)**2)/np.mean((f_true - np.mean(f_true))**2))
+    R2 = 1 - (np.mean((f_true - f_est) ** 2) / np.mean((f_true - np.mean(f_true)) ** 2))
 
-	return R2
+    return R2
 
 
 def mean_confidence_interval(data, confidence=0.95):
-    
-    a     = 1.0 * np.array(data)
-    n     = len(a)
+
+    a = 1.0 * np.array(data)
+    n = len(a)
     m, se = np.mean(a), sc.stats.sem(a)
-    h     = se * sc.stats.t.ppf((1 + confidence) / 2., n-1)
-    
+    h = se * sc.stats.t.ppf((1 + confidence) / 2.0, n - 1)
+
     return m, h
 
 
 def scrambled(orig):
-    
+
     dest = orig[:]
     np.random.shuffle(dest)
-    
+
     return dest
 
 
-def partition_(lst, n): 
-    
-    division = len(lst) / float(n) 
-    
-    return [lst[int(round(division * i)): int(round(division * (i + 1)))] for i in range(n)]
+def partition_(lst, n):
 
+    division = len(lst) / float(n)
 
-
+    return [
+        lst[int(round(division * i)) : int(round(division * (i + 1)))] for i in range(n)
+    ]
